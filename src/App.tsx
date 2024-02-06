b42089aae050c2cb109a29fe22247174701fdc39
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-02-06 13:33:26 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Header } from "./components/Header";
+import { Navbar } from "./components/Navbar";
+import { Profile } from "./components/Profile";
+
+export const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+    </header>
+  )
+}
